5351225a3ec78ce0bd32d0fc80e36446d50e86e3
Clara <clarazammar4@gmail.com>
2024-11-04 19:29:56 +0200

    Configured React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+/* STYLESHEETS */
+import "./styles/styles.css";
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
